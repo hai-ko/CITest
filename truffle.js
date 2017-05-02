ccddc8a708526b1a84cedea7c7825dd21d3866da
heiko <mail@heiko-burkhardt.de>
2017-05-02 13:51:57 +0200

    get path

--- truffle.js
+++ truffle.js
@@ -3,7 +3,8 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      from: "0x1ee4571be14b85107f8ce8a925bba278b75716d5"
     }
   }
 };
